8e0bd375494fac5084186f54199c21529e6fa706
ohliAnnie <ohliannie@naver.com>
2016-11-25 18:22:48 +0900

    reports - eventChart v1

--- public/scripts/report.js
+++ public/scripts/report.js
@@ -1,56 +1,62 @@
+  var eventChart = dc.pieChart('#eventChart');
 
-
-d3.json("/reports/NYX4", function(err, data){
+d3.json("/reports/restapi/getReportRawData", function(err, data){
   if(err) throw error;
-
-  data.forEach(function(d){
-    d.EVENT_TYPE = d.EVENT_TYPE;
-    console.log(d);
+     console.log(data.rtnData[0]);
+  data.rtnData[0].forEach(function(d){
+        var event = '';
+    switch(d.event_type){
+      case "1" :   // 피워
+        event = 'POWER';
+        break;   
+      case "17" :   // 조도
+        event = 'ALS';
+        break;    
+      case "33" :     // 진동
+        event = 'VIBRATION';
+        break;
+      case "49" :    // 노이즈
+        event = 'NOISE';
+        break;
+      case "65" :    // GPS
+        event = 'GPS';
+        break;
+      case "81" :     // 센서상태
+        event = 'STREET LIGHT';
+        break;
+      case "153" :    // 재부팅
+        event = 'REBOOT';
+        break;
+    }
+    d.event_name= event;
   });
 
-  var nyx = crossfilter(data);
+  var nyx = crossfilter(data.rtnData[0]);
   var all = nyx.groupAll();
 
   var eventDim = nyx.dimension(function(d) {
-    var event = '';
-    switch(d.EVENT_TYPE){
-      case 1 :
-        event = 'POWER';
-        break;
-      case 17 :
-        event = 'POWER';
-        break;
-      case 33 : 
-        event = 'POWER';
-        break;
-      case 49 :
-        event = 'POWER';
-        break;
-      case 65 :
-        event = 'GPS';
-        break;
-      case 81 : 
-        event = 'POWER';
-        break;
-      case 153 :
-        event = 'REBOOT';
-        break;
+    console.log(d.event_name);
+    return d.event_name;
+  });
 
-    }
-    console.log(d.EVENT_TYPE);
-    console.log(event);
-    return event;
-  })
-
-  var eventChart = dc.pieChart('#eventChart');
+  var eventGroup = eventDim.group();
 
   eventChart  /* dc.pieChart('#eventChart') */
-    .width(300)
-    .height(300)
-    .radius(200)
-    .dimension()
-    .group()
-    .label(function(d){
-    }) 
+    .radius(150)
+    .dimension(eventDim)
+    .group(eventGroup)
+    .label(function (d){
+        if(eventChart.hasFilter() && !eventChart.hasFilter(d.key)) {
+          return d.key + '(0%)';
+        }
+        var label = d.key;
+        console.log(d.key);
+        if(all.value()) {
+          label += label += '(' + Math.floor(d.value / all.value() * 100) + '%)';
+        }
+        return label;
+    });
+ //   eventChart.ordinalColors(['#3182bd', '#9ecae1', '#e6550d', '#fd8d3c', '#fdd0a2', '#31a354', '#a1d99b',  '#756bb1'])
 
+    dc.renderAll();
 });
