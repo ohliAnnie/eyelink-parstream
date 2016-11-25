0ec045ad414aa7c75e0a33c799fc8da024d8c2db
배성한 <furtur3@gmail.com>
2016-11-25 20:35:48 +0900

    ing bar chart

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -7,7 +7,7 @@ d3.json("/dashboard/restapi/getReportRawData", function(err, out_data) {
   var dateFormat = d3.time.format('%Y-%m-%d %H:%M:%S.s');
   var numberFormat = d3.format('.2f');
   var minDate  = new Date("2016-11-24");
-  var maxDate = new Date("2016-11-25");
+  var maxDate = new Date("2016-11-30");
 
   // {"node_id":"0001.00000001","event_time":"2016-11-25 15:42:34.0","event_type":"1",
   //  "active_power":19.618999481201172,"ampere":183.35400390625,"als_level":null,
@@ -16,60 +16,59 @@ d3.json("/dashboard/restapi/getReportRawData", function(err, out_data) {
 
   var data = out_data.rtnData[0];
   data.forEach(function (d) {
-    console.log(d);
     d.dd = dateFormat.parse(d.event_time);
     // d.month = d3.time.month(d.dd); // pre-calculate month for better performance
     // d.VOLTAGE = +d.VOLTAGE;
-    d.AMPERE = +d.ampere;
+    d.ampere = +d.ampere;
     // d.POWER_FACTOR = +d.POWER_FACTOR;
-    d.ACTIVE_POWER = +d.active_power;
+    d.active_power = +d.active_power;
     // d.REACTIVE_POWER = +d.REACTIVE_POWER;
     // d.APPARENT_POWER = +d.APPARENT_POWER;
     // d.AMOUNT_OF_ACTIVE_POWER = +d.AMOUNT_OF_ACTIVE_POWER;
-    d.check = +0.85;
-    console.log(d);
+    // d.check = +0.85;
+    // console.log(d);
   });
 
-  var nyx = crossfilter(data);
-  var all = nyx.groupAll();
+  var ndx = crossfilter(data);
 
-  var fluctuation = nyx.dimension(function (d) {
-      return d.AMOUNT_OF_ACTIVE_POWER * 100;
+  console.log(ndx);
+  var apDim = ndx.dimension(function (d) {
+      return d.active_power * 100;
+  });
+  console.log(apDim);
+  var ampereGroup = apDim.group().reduceSum(function(d) {
+    console.log('ampereGroup');
+    return d.active_power * d.ampere / 1000;
   });
 
-  var fluctuationGroup = fluctuation.group();
+  console.log(ampereGroup);
 
-    // Create a bar chart and use the given css selector as anchor. You can also specify
-    // an optional chart group for this chart to be scoped within. When a chart belongs
-    // to a specific group then any interaction with such chart will only trigger redraw
-    // on other charts within the same chart group.
-    // <br>API: [Bar Chart](https://github.com/dc-js/dc.js/blob/master/web/docs/api-latest.md#bar-chart)
-  fluctuationChart /* dc.barChart('#volume-month-chart', 'chartGroup') */
+  fluctuationChart
     .width(420)
     .height(180)
     .margins({top: 10, right: 50, bottom: 30, left: 40})
-    .dimension(fluctuation)
-    .group(fluctuationGroup)
-    .elasticY(true)
-    // (_optional_) whether bar should be center to its x value. Not needed for ordinal chart, `default=false`
-    .centerBar(true)
-    // (_optional_) set gap between bars manually in px, `default=2`
-    .gap(1)
-    // (_optional_) set filter brush rounding
-    .round(dc.round.floor)
-    .alwaysUseRounding(true)
+    .dimension(apDim)
+    .group(ampereGroup)
+    // .elasticY(true)
+    // // (_optional_) whether bar should be center to its x value. Not needed for ordinal chart, `default=false`
+    // .centerBar(true)
+    // // (_optional_) set gap between bars manually in px, `default=2`
+    // .gap(1)
+    // // (_optional_) set filter brush rounding
+    // .round(dc.round.floor)
+    // .alwaysUseRounding(true)
     .x(d3.scale.linear().domain([-25, 25]))
-    .renderHorizontalGridLines(true)
+    // .renderHorizontalGridLines(true)
     // Customize the filter displayed in the control span
-    .filterPrinter(function (filters) {
-        var filter = filters[0], s = '';
-        s += numberFormat(filter[0]) + '% -> ' + numberFormat(filter[1]) + '%';
-        return s;
-    });
+    // .filterPrinter(function (filters) {
+    //     var filter = filters[0], s = '';
+    //     s += numberFormat(filter[0]) + '% -> ' + numberFormat(filter[1]) + '%';
+    //     return s;
+    // });
 
-    // Customize axes
-    fluctuationChart.xAxis().tickFormat(
-        function (v) { return v + '%'; });
-    fluctuationChart.yAxis().ticks(5);
-
+    // // Customize axes
+    // fluctuationChart.xAxis().tickFormat(
+    //     function (v) { return v + '%'; });
+    // fluctuationChart.yAxis().ticks(5);
+  fluctuationChart.render();
 });
