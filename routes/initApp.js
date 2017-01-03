6234305d5ebc25a85b9efe407545a23bd8b33cd1
TovenBae <furtur3@gmail.com>
2017-01-03 21:14:30 +0900

    fix global variable

--- routes/initApp.js
+++ routes/initApp.js
@@ -1,5 +1,4 @@
 var CONSTS = require('./consts');
-var config = require('../config/config.json');
 require('date-utils');
 
 
@@ -12,10 +11,10 @@ function loadData(callback) {
   var d = new Date();
   console.log('initApps/loadData -> today : %s', Date.today());
 
-  for (var i=0; i<config.loaddataonstartup.loading_day; i++) {
+  for (var i=0; i<global.config.loaddataonstartup.loading_day; i++) {
     console.log('initApps/loadData -> day : %s', d.removeDays(1).toFormat('YYYY-MM-DD'));
     var flag = 'R';
-    if (i === (config.loaddataonstartup.loading_day-1))
+    if (i === (global.config.loaddataonstartup.loading_day-1))
       flag = 'C';
     // console.log('initApps/loadData -> i :   %s, loadind day : %s, flag : %s', i, (CONSTS.CONFIG.LOADING_DAY-1), flag);
     var vDate = d.toFormat('YYYY-MM-DD');
