4c742e7627cadafbcd9786b30414a1d3604912de
TovenBae <furtur3@gmail.com>
2017-01-09 16:45:34 +0900

    change db pool -> createClient

--- routes/dao/parstream/nodelib-dashboard.js
+++ routes/dao/parstream/nodelib-dashboard.js
@@ -1,12 +1,13 @@
 var Utils = require('../../util');
 
 var parstream = require("parstream")
-var parstream =  parstream.createPool({
+var opts = {
     host: 'm2u-parstream.eastus.cloudapp.azure.com',
     // host: 'm2u-da.eastus.cloudapp.azure.com',
     port: 9042,
     size: 5,
-});
+}
+// var parstream =  parstream.createPool(opts);
 
 var sqlList = {
   // Event 발생 건수 조회
@@ -99,13 +100,16 @@ DashboardProvider.prototype.selectSingleQueryByID = function (queryId, datas, ca
   console.time('nodelib-Dashboard/selectSingleQueryByID -> '+ queryId +' total ');
   console.log('nodelib-Dashboard/selectSingleQueryByID -> (%s) queryID', queryId)
 
+  // no pool method
+  var parstream = require('parstream').createClient(opts);
+
   parstream.connect(function(err) {
     if (err) {
       callback(err);
     } else {
       // SQL 내 파라메타를 변경해준다.
       var sSql = Utils.replaceSql(sqlList[queryId], datas);
-      // console.log('nodelib-Dashboard/selectSingleQueryByID -> ' + sSql);
+      console.log('nodelib-Dashboard/selectSingleQueryByID -> ' + sSql);
 
       console.time('nodelib-Dashboard/selectSingleQueryByID -> ('+ queryId +') executeQuery');
       parstream.query(sSql, function(err, resultset) {
@@ -119,12 +123,14 @@ DashboardProvider.prototype.selectSingleQueryByID = function (queryId, datas, ca
           console.log('nodelib-Dashboard/selectSingleQueryByID -> (%s) resultset error : %s', queryId, (typeof resultset.error === 'string'));
           err = resultset.error;
           console.timeEnd('nodelib-Dashboard/selectSingleQueryByID -> '+ queryId +' total ');
+          parstream.close();
           callback(resultset.error, [[]], datas);
         } else {
           console.log('nodelib-Dashboard/selectSingleQueryByID -> (%s) resultset.rows.length : %d', queryId, resultset.rows.length);
           // console.log('nodelib-Dashboard/selectSingleQueryByID -> resultset type : %s', (typeof resultset === 'object'));
           // console.log('nodelib-Dashboard/selectSingleQueryByID -> resultset rows type : %s', (typeof resultset.rows === 'object'));
           console.timeEnd('nodelib-Dashboard/selectSingleQueryByID -> '+ queryId +' total ');
+          parstream.close();
           callback(err, [resultset.rows], datas);
         }
         // parstream.close(function () {
