e461c281413aa77ef8d3a2f9c67819479b363f53
ohliAnnie <ohliannie@naver.com>
2017-01-31 18:36:05 +0900

    2017-01-31

--- routes/nodeCon.js
+++ routes/nodeCon.js
@@ -1,3 +1,4 @@
+var CONSTS = require('./consts');
 var express = require('express');
 var router = express.Router();
 
@@ -5,25 +6,33 @@ var QueryProvider = require('./dao/' + global.config.fetchData.database + '/'+ c
 var queryProvider = new QueryProvider();
 
 var mainmenu = {home: 'is-selected', info: '', job: '', staff: '', consult: '', event: ''};
+
 /* GET home page. */
-router.get('/nodes', function(req, res, next) {
+ router.get('/nodes', function(req, res, next) {
   console.log('node');
      var in_data = {};
-  queryProvider.selectSingleQueryByID("reports","selectNodeList", in_data, function(err, out_data, params) {
+  queryProvider.selectSingleQueryByID("node","selectNodeList", in_data, function(err, out_data, params) {
     // console.log(out_data);
     var rtnCode = CONSTS.getErrData('0000');
     if (out_data[0] === null) {
       rtnCode = CONSTS.getErrData('0001');
     }
-    console.log('node/restapi/getNodeList -> length : %s', out_data[0].length);
   res.render('./node/nodes', { title: 'EyeLink for ParStream' , nodeData:out_data[0]});
     });
 });
 
 router.get('/status', function(req, res, next) {
   console.log('node');
-  // res.redirect('/dashboard/');
-  res.render('./node/node_status', { title: 'EyeLink for ParStream' });
+    var in_data = {};
+  queryProvider.selectSingleQueryByID("node","selectNodeList", in_data, function(err, out_data, params) {
+    // console.log(out_data);
+    var rtnCode = CONSTS.getErrData('0000');
+    if (out_data[0] === null) {
+      rtnCode = CONSTS.getErrData('0001');
+    }
+    console.log(out_data[0]);
+  res.render('./node/node_status', { title: 'EyeLink for ParStream' , nodeData:out_data[0]});  
+     });
 });
 
 router.get('/als', function(req, res, next) {
@@ -38,16 +47,17 @@ router.get('/gps', function(req, res, next) {
   res.render('./node/registration_gps', { title: 'EyeLink for ParStream' });
 });
 
-// query Report
-router.get('/restapi/getReportRawData', function(req, res, next) {
-  console.log('reports/restapi/getReportRawData');
-  var in_data = {MERGE:'Y'};
-  queryProvider.selectSingleQueryByID("node","selectEventRawData", in_data, function(err, out_data, params) {
+
+router.get('/restapi/getNodeList', function(req, res, next) {
+  console.log(req.query);
+  var in_data = {};
+  queryProvider.selectSingleQueryByID("node", "selectNodeList", in_data, function(err, out_data, params) {
     // console.log(out_data);
     var rtnCode = CONSTS.getErrData('0000');
-    if (out_data == null) {
+    if (out_data[0] === null) {
       rtnCode = CONSTS.getErrData('0001');
     }
+    console.log('node/restapi/getNodeList -> length : %s', out_data[0].length);
     res.json({rtnCode: rtnCode, rtnData: out_data[0]});
   });
 });
