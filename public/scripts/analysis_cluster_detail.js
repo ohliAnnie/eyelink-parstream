6799491c45a50b21313edc132c541c25a26b26ec
ohliAnnie <ohliannie@naver.com>
2017-02-02 16:33:55 +0900

    2017-02-02

---
+++ public/scripts/analysis_cluster_detail.js
@@ -0,0 +1,78 @@
+function getMasterList() {
+  var sdate = $('#sdate').val();
+  var edate = $('#edate').val();  
+   console.log('%s, %s', sdate, edate);
+   $.ajax({
+    url: "/analysis/restapi/getDaClusterMaster" ,
+    dataType: "json",
+    type: "get",
+    data: {startDate:sdate, endDate:edate},
+    success: function(result) {
+      // console.log(result);
+      if (result.rtnCode.code == "0000") {
+        var master = result.rtnData;
+        console.log(master);
+        drawMaster(master);
+      } else {
+        //- $("#errormsg").html(result.message);
+      }
+    },
+    error: function(req, status, err) {
+      //- alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+      $("#errormsg").html("code:"+status+"\n"+"message:"+req.responseText+"\n"+"error:"+err);
+    }
+  });
+}
+
+function drawMaster(master) { 
+  var seatvar = document.getElementsByClassName("tblClusterDir");           
+        console.log(seatvar);
+        console.log(master);
+  nodeList.forEach(function(d) {  
+
+    var sb = new StringBuffer();
+    var a = d.c0.split(':');
+    var script = "javascript:getNodePower('"+d.c0+"');";
+    sb.append('<tr><td><span class="bold theme-fone"><a href="'+script+'"> Cluster0 </a></span></td><td></td></tr>');
+    
+    for(var i=0; i < a.length; i++) {
+      sb.append('<tr><td></td><td>');
+      var script = "javascript:clickNode('"+a[i]+"');";
+      sb.append('<a class="primary-link" href="'+script+'">' + a[i] + '</a>');
+      sb.append('</td></tr>');
+    }
+    var a = d.c1.split(':');
+    var script = "javascript:getNodePower('"+d.c1+"');";
+    sb.append('<tr><td><span class="bold theme-fone"><a href="'+script+'"> Cluster1 </a></span></td><td></td></tr>');    
+    for(var i=0; i < a.length; i++) {
+      sb.append('<tr><td></td><td>');
+      var script = "javascript:clickNode('"+a[i]+"');";      
+      sb.append('<a class="primary-link" href="'+script+'">' + a[i] + '</a>');
+      sb.append('</td></tr>');
+    }
+    var a = d.c2.split(':');
+    var script = "javascript:getNodePower('"+d.c2+"');";
+    sb.append('<tr><td><span class="bold theme-fone"><a href="'+script+'"> Cluster2 </a></span></td><td></td></tr>');
+    var a = d.c2.split(':');
+    for(var i=0; i < a.length; i++) {
+      sb.append('<tr><td></td><td>');
+      var script = "javascript:clickNode('"+a[i]+"');";      
+      sb.append('<a class="primary-link" href="'+script+'">' + a[i] + '</a>');
+      sb.append('</td></tr>');
+    }
+    var a = d.c3.split(':');
+    var script = "javascript:getNodePower('"+d.c0+"');";
+    sb.append('<tr><td><span class="bold theme-fone"><a href="'+script+'"> Cluster3 </a></span></td><td></td></tr>');
+    var a = d.c3.split(':');
+    for(var i=0; i < a.length; i++) {
+      sb.append('<tr><td></td><td>');
+      var script = "javascript:clickNode('"+a[i]+"');";
+      sb.append('<a class="primary-link" href="'+script+'">' + a[i] + '</a>');
+      sb.append('</td></tr>');
+    }
+    console.log('sb : %s', sb.toString());
+    $('#tblClusterDir').append(sb.toString());
+  });
+    
+
+}
--- routes/nodeLogin.js
+++ routes/nodeLogin.js
@@ -15,15 +15,48 @@ router.get('/', function(req, res, next) {
   res.render('./login/login', { title: 'EyeLink for ParStream' });
 });
 
+// login
+router.get('/login', function(req, res, next) {    
+
+   var in_data = {              
+      USERID: req.query.userid,                    
+   };
+  console.log('login');  
+   queryProvider.selectSingleQueryByID("user", "selectLogin", in_data, function(err, out_data, params) {
+    console.log(out_data);
+    var rtnCode = CONSTS.getErrData('0000');
+    if (out_data[0] === null) {
+      rtnCode = CONSTS.getErrData('0001');
+      console.log('등록된 정보가 없어용')
+      res.render('/', { title: 'EyeLink for ParStream' });      
+    } else {
+      var in_data = {              
+        USERID: req.query.userid,                    
+        REGDATE: out_data[0][0].reg_date
+      };
+      console.log(in_data);
+      queryProvider.selectSingleQueryByID("user", "selectPassword", in_data, function(err, out_data, params) {
+        console.log(out_data);
+        console.log('password : ' + out_data[0][0].user_pw);
+        if(req.query.password === out_data[0][0].user_pw) {
+          console.log('로그인 축축');
+          res.redirect('/dashboard/');
+        } else {
+          console.log('비밀번호 틀렸돵');
+          res.redirect('/');
+        }
+      });
+    } 
+  });  
+});
+
 // create
 router.post("/", function(req, res){     
  var in_data = {              
       USERID: req.body.userid,        
   };
-  queryProvider.selectSingleQueryByID("user", "selectCheckJoin", in_data, function(err, out_data, params) {    
-   var rtnCode = CONSTS.getErrData('0000');
+  queryProvider.selectSingleQueryByID("user", "selectCheckJoin", in_data, function(err, out_data, params) {       
     if (out_data[0] === null) {
-      rtnCode = CONSTS.getErrData('0001');     
       console.log(req.body);
        var in_data = {              
             USERNAME: req.body.username,
@@ -35,10 +68,11 @@ router.post("/", function(req, res){
     console.log(in_data);
       queryProvider.selectSingleQueryByID("user", "insertUser", in_data, function(err, out_data, params) {    
         res.redirect("/");
-     });
-  } else {
-     res.render('', { error : '이미 ID로 등록된 ID입니다.'})
-  }       
+      });
+    } else {
+      console.log('이미 있는 아이디닷');
+       res.render("/", { error : '이미 ID로 등록된 ID입니다.'})
+    }       
    res.redirect("/");  
   }); 
 });
