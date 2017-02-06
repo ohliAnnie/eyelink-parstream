1760b26dfa61e15a63ece2c587e0ddb6565c422d
TovenBae <furtur3@gmail.com>
2017-02-06 17:03:03 +0900

    python socket test 모듈 추가

--- test/testSocketio.js
+++ test/testSocketio.js
@@ -3,6 +3,7 @@ var should = require('should');
 var assert = require("assert")
 var request = require("supertest");
 var expect = require("chai").expect;
+var net = require('net');
 
 var io = require('socket.io-client');
 
@@ -67,26 +68,58 @@ describe("Socketio", function(){
   });
 
   describe("Python -> ", function() {
+    it.only('TO-DO Socket Data 전송', function(done) {
 
-    it('TO-DO Socket Data 전송', function(done) {
-      var client1 = io.connect(socketURL, options);
-
-      var count = 0;
-      client1.on('refreshData', function(data){
-        console.log(data);
-        data.count.should.equal(++count);
-        // if (data == 5)
-        //   done();
-
-        client1.emit('getEventListForAlarm', 0);
-      });
-
-      client1.on('sendEventListForAlarm', function(data) {
-        console.log(data);
-        done();
-      });
+      var Dwarves = getConnection("Dwarves");
+      var Elves = getConnection("Elves");
+      var Hobbits = getConnection("Hobbits");
+      writeData(Dwarves, "More Axes");
+      writeData(Elves, "More Arrows");
+      writeData(Hobbits, "More Pipe Weed");
     })
 
 
   });
 });
+
+function getConnection(connName){
+  var pUrl = '192.168.10.27';
+  // var pUrl = 'localhost';
+  var pPort = 50007;
+  var client = net.connect({port: pPort, host:pUrl}, function() {
+    console.log(connName + ' Connected: ');
+    console.log('   local = %s:%s', this.localAddress, this.localPort);
+    console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
+    this.setTimeout(500);
+    this.setEncoding('utf8');
+    this.on('data', function(data) {
+      console.log(connName + " From Server: " + data.toString());
+      this.end();
+    });
+    this.on('end', function() {
+      console.log(connName + ' Client disconnected');
+    });
+    this.on('error', function(err) {
+      console.log('Socket Error: ', JSON.stringify(err));
+    });
+    this.on('timeout', function() {
+      console.log('Socket Timed Out');
+    });
+    this.on('close', function() {
+      console.log('Socket Closed');
+    });
+  });
+  return client;
+}
+
+function writeData(socket, data){
+  var success = !socket.write(data);
+  if (!success){
+    (function(socket, data){
+      socket.once('drain', function(){
+        writeData(socket, data);
+      });
+    })(socket, data);
+  }
+}
+
