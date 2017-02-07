2cb048aaaa9b13520c6d342c838d352995bf1e42
ohliAnnie <ohliannie@naver.com>
2017-02-07 18:09:37 +0900

    2017-02-07

--- routes/nodeManagement.js
+++ routes/nodeManagement.js
@@ -6,22 +6,71 @@ var router = express.Router();
 var QueryProvider = require('./dao/' + global.config.fetchData.database + '/'+ config.fetchData.method + '-db').QueryProvider;
 var queryProvider = new QueryProvider();
 
-var mainmenu = {dashboard:'', reports:'', timeseries:'', management: ' open selected', users:'', settings:''};
+var mainmenu = {dashboard:'', timeseries:'', reports:'', analysis:'', management: 'open selected', settings:''};
 
 
 /* GET reports page. */
 router.get('/', function(req, res, next) {
   // console.log(_rawDataByDay);
-  mainmenu.dashboard = ' open selected';
-  mainmenu.timeseries = '';
   res.render('./dashboard/main', { title: 'EyeLink for ParStream', mainmenu:mainmenu});
 });
 
 router.get('/timeseries', function(req, res, next) {
   // console.log(_rawDataByDay);
-  mainmenu.dashboard = '';
-  mainmenu.timeseries = ' open selected';
   res.render('./dashboard/timeseries', { title: 'EyeLink for ParStream', mainmenu:mainmenu });
 });
+
+router.get('/user', function(req, res, next) {
+  // console.log(_rawDataByDay);
+  var in_data = {};
+   queryProvider.selectSingleQueryByID("user", "selectUserList", in_data, function(err, out_data, params) {
+      var rtnCode = CONSTS.getErrData('0000');
+      if (out_data[0] === null) {
+        rtnCode = CONSTS.getErrData('0001');
+      }
+      console.log(out_data[0]);    
+      var users = out_data[0];      
+      res.render('./management/user_list', { title: 'EyeLink for ParStream', mainmenu:mainmenu, users:users });
+   });  
+});
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('./management/sign_up', { title: 'EyeLink for ParStream', mainmenu:mainmenu });
+});
+
+var joinCnt = 0;
+// create
+router.post('/sign_up', function(req, res) {
+  var in_data = {              
+    USERID: req.body.userid,        
+  };
+  queryProvider.selectSingleQueryByID("user", "selectCheckJoin", in_data, function(err, out_data, params) {           
+    console.log(out_data[0][0]);
+    console.log(req.body.userid);
+    if (out_data[0][0] != null){
+      console.log('이미 있는 아이디닷');
+      var err = "이미 등록된 아이디 입니다.";      
+       res.redirect("./sign_up?msg=3");
+    }  else  {    
+     if(joinCnt == 0){
+      console.log('joinCnt : '+joinCnt);
+       var in_data = {              
+            USERNAME: req.body.username,
+            USERID: req.body.userid,        
+            PASSWORD: req.body.password[0],
+            EMAIL: req.body.email,  
+            USERROLE: req.body.userrole,     
+        };
+        joinCnt++;
+        console.log('joinCnt : '+joinCnt);
+        queryProvider.selectSingleQueryByID("user", "insertUser", in_data, function(err, out_data, params) {            
+
+          console.log('회원가입 축축');  
+          res.redirect("./sign_up?msg=4");
+        });       
+       //res.render('./management/users', { title: 'EyeLink for ParStream', mainmenu:mainmenu });
+    } 
+  }); 
+}); 
 
 module.exports = router;
