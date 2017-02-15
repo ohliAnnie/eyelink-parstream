466a80f9420e353feda637e396ee5a39a4b420c9
ohliAnnie <ohliannie@naver.com>
2017-02-15 11:59:19 +0900

    run Analysis

--- public/scripts/analysis_clustering.js
+++ public/scripts/analysis_clustering.js
@@ -1,17 +1,17 @@
-function runAnalysis() {
+function runAnalysis(interval) {
   var sdate = $('#sdate').val();
-  var edate = $('#edate').val();  
+  var edate = $('#edate').val();    
   // TO-DO message config로 처리함.
   if (confirm("분석에 시간이 걸릴수 있어 Background로 작업이 수행됩니다.\n 진행하시겠습니까? ")) {
     $.ajax({
         url: "/analysis/restapi/runAnalysis" ,
         dataType: "json",
         type: "post",
-        data: { startDate:sdate, endDate:edate  },
+        data: { startDate:sdate, endDate:edate , interval:interval },
         success: function(result) {
           if (result.rtnCode.code == "0000") {
             var master = result.rtnData;
-            console.log(master);
+            console.log(master);            
           }
         },
         error: function(req, status, err) {
@@ -51,6 +51,7 @@ function getMasterList(interval) {
       success: function(result) {
         if (result.rtnCode.code == "0000") {
           var master = result.rtnData;          
+          console.log('interval : '+interval)
           console.log(master);
           drawMaster(master);
         }
