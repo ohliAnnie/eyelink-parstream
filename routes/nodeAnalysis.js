afde95e0ca4b77088a073ff57d29117a728c18fb
ohliAnnie <ohliannie@naver.com>
2017-02-15 10:55:58 +0900

    analysis & user

--- routes/nodeAnalysis.js
+++ routes/nodeAnalysis.js
@@ -78,9 +78,12 @@ router.get('/restapi/getDaClusterMaster', function(req, res, next) {
       END_TIMESTAMP: req.query.endDate + ' 23:59:59',
       INTERVAL: parseInt(req.query.interval),
       FLAG : 'N'};
-      console.log('indata');
-console.log(in_data);
-  queryProvider.selectSingleQueryByID("analysis", "selectDaClusterMaster", in_data, function(err, out_data, params) {
+  if(req.query.interval === 'all')  {
+    var sql = "selectDaClusterMasterAll";
+  } else {
+    var sql = "selectDaClusterMaster";
+  }
+  queryProvider.selectSingleQueryByID("analysis", sql, in_data, function(err, out_data, params) {
     // console.log(out_data);
     var rtnCode = CONSTS.getErrData('0000');
     if (out_data[0] === null) {
