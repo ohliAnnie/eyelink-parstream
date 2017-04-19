d21053a1c6e524e7719b1ee266ed0295d877cf92
ohliAnnie <ohliannie@naver.com>
2017-04-19 14:58:09 +0900

    service to sample

---
+++ routes/nodeSample.js
@@ -0,0 +1,37 @@
+var CONSTS = require('./consts');
+var Utils = require('./util');
+var express = require('express');
+var router = express.Router();
+
+var mainmenu = {dashboard:'', timeseries:'', reports:'', analysis:'', management:'', settings:'', sample:'open selected'};
+
+/* GET reports page. */
+router.get('/', function(req, res, next) {
+  res.render('./sample/serverMap', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/serverMap', function(req, res, next) {
+  res.render('./sample/serverMap', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/cytoscape01', function(req, res, next) {
+  res.render('./sample/cytoscape01', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/cytoscape02', function(req, res, next) {
+  res.render('./sample/cytoscape02', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/cytoscape03', function(req, res, next) {
+  res.render('./sample/cytoscape03', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/cytoscape04', function(req, res, next) {
+  res.render('./sample/cytoscape04', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+
+router.get('/cytoscape05', function(req, res, next) {
+  res.render('./sample/cytoscape05', { title: 'EyeLink for Service Monitoring', mainmenu:mainmenu });
+});
+  
+module.exports = router;
