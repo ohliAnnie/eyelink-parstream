25dc99d4f0d820349cff1334367bbb5756ac20ec
ohliAnnie <ohliannie@naver.com>
2017-06-22 16:41:19 +0900

    Service / Call Tree

--- public/scripts/calltreeEFSM.js
+++ public/scripts/calltreeEFSM.js
@@ -22,12 +22,14 @@ function getTransaction(id, date) {
     }
   });
 }
-function drawDetail(detail) {  
+function drawDetail2(detail) {  
   console.log(detail);
   $('#call').empty();
-  var sb = new StringBuffer();
-  sb.append('<tr><th>Method</th><th>Argument</th><th>Start Time</th><th>Gap(ms)</th>');
-  sb.append('<th>Exec(ms)</th><th>Exec(%)</th><th>Self(ms)</th><th>Class</th><th>API</th><th>Agent</th><th>Application</th></tr>');  
+  var sb = '<div class="row"><div class="col-md-12"><div class="portlet light bordered"><div class="portlet-body form">';
+  sb += '<table class="able tree-2 table-bordered table-striped table-condensed">';
+  sb += '<tr><th>Method</th><th>Argument</th><th>Start Time</th><th>Gap(ms)</th>';
+  sb += '<th>Exec(ms)</th><th>Exec(%)</th><th>Self(ms)</th><th>Class</th><th>API</th><th>Agent</th><th>Application</th></tr>';  
+  var level = 1;
   detail.callstack.forEach(function(d){
     if(d.start_time==null) {
       d.start_time = '';
@@ -53,19 +55,69 @@ function drawDetail(detail) {
     if(d.agent_id==null) {
       d.agent_id = '';
     }
-    if(d.applcation_id==null) {
-      d.applcation_id = '';
+    if(d.application_id==null) {
+      d.application_id = '';
     }
     console.log(d);
     if(d.level == 1){
-      sb.append('<tr class="treegrid-'+d.seq+'">');
+      sb += '<tr class="treegrid-'+d.seq+'">';
+      level = d.seq;z
+    } else if(d.level != (level+1)){  
+      sb += '<tr class="treegrid-'+d.seq+' treegrid-parent-'+level+'">';
+      level = d.seq;
     } else {
-      sb.append('<tr class="treegrid-'+d.seq+' treegrid-parent-'+d.pseq+'">');
+      sb += '<tr class="treegrid-'+d.seq+' treegrid-parent-'+level+'">';
+    }
+    sb += '<td>'+d.method+'</td><td>'+d.argument+'</td><td>'+d.start_time+'</td><td>'+d.gap_time+'</td>';
+    sb += '<td>'+d.exec_time+'</td><td></td><td>'+d.self_time+'</td><td>'+d.exec_class+'</td>';
+    sb += '<td>'+d.exec_api+'</td><td id="id">'+d.agent_id+'</td><td>'+d.application_id+'</td></tr>';
+  });
+  sb+= '</table></div></div></div></div>';
+  console.log(sb);
+  document.getElementById("call").outerHTML = sb;
+  $('.tree-2').treegrid({
+    expanderExpandedClass: 'glyphicon glyphicon-minus',
+    expanderCollapsedClass: 'glyphicon glyphicon-plus'
+  });
+}
+
+function drawDetail(detail) {  
+  console.log(detail);
+  $('#call').empty();
+  var sb = new StringBuffer();
+  sb.append('<div class="row"><div class="col-md-12"><div class="portlet light bordered"><div class="portlet-body form">');
+  sb.append('<table class="table tree-2 table-bordered table-striped table-condensed">');
+  sb.append('<tr><th>Method</th><th>Argument</th><th>Start Time</th><th>Gap(ms)</th>');
+  sb.append('<th>Exec(ms)</th><th>Exec(%)</th><th>Self(ms)</th><th>Class</th><th>API</th><th>Agent</th><th>Application</th></tr>');  
+  var level = 1;
+  detail.callstack.forEach(function(d){
+    if(d.start_time==null) {      d.start_time = '';    }    
+    if(d.gap_time==null) {      d.gap_time = '';    }
+    if(d.exec_time==null) {      d.exec_time = '';    }
+    if(d.self_time==null) {      d.self_time = '';    }
+    if(d.exec_class==null) {      d.exec_class = '';    }
+    if(d.exec_time==null) {      d.exec_time = '';    }
+    if(d.exec_api==null) {      d.exec_api = '';    }
+    if(d.agent_id==null) {      d.agent_id = '';    }
+    if(d.application_id==null) {      d.application_id = '';    }    
+    if(d.level == 1){
+      sb.append('<tr class="treegrid-'+d.seq+'">');
+      level = d.seq;
+    } else if(d.level != (level+1)){  
+      sb.append('<tr class="treegrid-'+d.seq+' treegrid-parent-'+level+'">');
+      level = d.seq;
+    } else {
+      sb.append('<tr class="treegrid-'+d.seq+' treegrid-parent-'+level+'">');
     }
     sb.append('<td>'+d.method+'</td><td>'+d.argument+'</td><td>'+d.start_time+'</td><td>'+d.gap_time+'</td>')
     sb.append('<td>'+d.exec_time+'</td><td></td><td>'+d.self_time+'</td><td>'+d.exec_class+'</td>');
-    sb.append('<td>'+d.exec_api+'</td><td>'+d.agent_id+'</td><td>'+d.applcation_id+'</td></tr>');
-
+    sb.append('<td>'+d.exec_api+'</td><td>'+d.agent_id+'</td><td>'+d.application_id+'</td></tr>');
   });
-  $('#call').append(sb.toString());
+  sb.append('</table></dir></dir></dir></dir>');
+  console.log(sb.toString());
+  $('#call').append(sb.toString());  
+    $('.tree-2').treegrid({
+    expanderExpandedClass: 'glyphicon glyphicon-minus',
+    expanderCollapsedClass: 'glyphicon glyphicon-plus'
+  }); 
 }
