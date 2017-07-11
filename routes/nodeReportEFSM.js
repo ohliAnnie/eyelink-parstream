d51330ac3deef9552e475f2cb52e22d606f7ad8b
ohliAnnie <ohliannie@naver.com>
2017-07-11 18:22:43 +0900

    기간 범위 수정

--- routes/nodeReportEFSM.js
+++ routes/nodeReportEFSM.js
@@ -38,23 +38,6 @@ router.get('/test', function(req, res, next) {
   res.render('./reports/test', { title: global.config.productname, mainmenu:mainmenu });
 });
 
-router.get('/power', function(req, res, next) {
-  res.render('./reports/report_power', { title: global.config.productname, mainmenu:mainmenu });
-});
-
-router.get('/fault', function(req, res, next) {
-  res.render('./reports/fault_notification', { title: global.config.productname, mainmenu:mainmenu });
-});
-
-router.get('/d3', function(req, res, next) {
-  res.render('./report_d3', { title: 'Report_d3', mainmenu:mainmenu });
-});
-
-router.get('/live', function(req, res, next) {
-  res.render('./reports/report_live', { title: global.config.productname, mainmenu:mainmenu });
-});
-
-
 
 // query Report
 router.get('/restapi/getJiraAcc', function(req, res, next) {
@@ -101,6 +84,25 @@ router.get('/restapi/getProcessList', function(req, res, next) {
     lte : req.query.lte
   };
   queryProvider.selectSingleQueryByID2("reports","selectProcessList", in_data, function(err, out_data, params) {
+    // console.log(out_data);
+    var rtnCode = CONSTS.getErrData('0000');
+    if (out_data == null) {
+      rtnCode = CONSTS.getErrData('0001');
+    }       
+    res.json({rtnCode: rtnCode, rtnData: out_data});
+  });
+});
+
+// query Report
+router.get('/restapi/getProcessListByName', function(req, res, next) {
+  console.log('reports/restapi/getProcessListByName');
+  var in_data = {
+    index : req.query.index,
+    gte : req.query.gte,
+    lte : req.query.lte,
+    name : req.query.name
+  };
+  queryProvider.selectSingleQueryByID2("reports","selectProcessListByName", in_data, function(err, out_data, params) {
     // console.log(out_data);
     var rtnCode = CONSTS.getErrData('0000');
     if (out_data == null) {
@@ -166,61 +168,5 @@ router.get('/restapi/getAccessError', function(req, res, next) {
   });
 });
 
-// query RawData
-router.get('/restapi/getTbRawDataByPeriod', function(req, res, next) {
-  console.log(req.query);
-   var in_data = {
-      START_TIMESTAMP: req.query.startDate + ' 00:00:00',
-      END_TIMESTAMP: req.query.endDate + ' 23:59:59',
-      FLAG : 'N'};
-  queryProvider.selectSingleQueryByID("reports","selectEventRawDataOld", in_data, function(err, out_data, params) {
-    // console.log(out_data);
-    var rtnCode = CONSTS.getErrData('0000');
-    if (out_data[0] === null) {
-      rtnCode = CONSTS.getErrData('0001');
-    }
-
-    console.log('reports/restapi/getTbRawDataByPeriod -> length : %s', out_data[0].length);
-    res.json({rtnCode: rtnCode, rtnData: out_data[0]});
-  });
-});
-
-// query RawData Power
-router.get('/restapi/getTbRawDataByPeriodPower', function(req, res, next) {
-  console.log(req.query);
-   var in_data = {
-      START_TIMESTAMP: req.query.startDate + ' 00:00:00',
-      END_TIMESTAMP: req.query.endDate + ' 23:59:59',
-      FLAG : 'N'};
-  queryProvider.selectSingleQueryByID("reports","selectEventRawDataPower", in_data, function(err, out_data, params) {
-    // console.log(out_data);
-    var rtnCode = CONSTS.getErrData('0000');
-    if (out_data[0] === null) {
-      rtnCode = CONSTS.getErrData('0001');
-    }
-
-    console.log('reports/restapi/getTbRawDataByPeriodPower -> length : %s', out_data[0].length);
-    res.json({rtnCode: rtnCode, rtnData: out_data[0]});
-  });
-});
-
-// query RawData Power All
-router.get('/restapi/getTbRawDataByAllPower', function(req, res, next) {
-  console.log(req.query);
-   var in_data = {
-      START_TIMESTAMP: req.query.startDate + ' 00:00:00',
-      END_TIMESTAMP: req.query.endDate + ' 23:59:59',
-      FLAG : 'N'};
-  queryProvider.selectSingleQueryByID("reports","selectEventRawDataPowerAll", in_data, function(err, out_data, params) {
-    // console.log(out_data);
-    var rtnCode = CONSTS.getErrData('0000');
-    if (out_data[0] === null) {
-      rtnCode = CONSTS.getErrData('0001');
-    }
-
-    console.log('reports/restapi/getTbRawDataByAllPower -> length : %s', out_data[0].length);
-    res.json({rtnCode: rtnCode, rtnData: out_data[0]});
-  });
-});
 
 module.exports = router;
