a7441d07f79ac7ab0776624773c365ca568bb64a
ohliAnnie <ohliannie@naver.com>
2017-08-18 15:18:48 +0900

    sankey 수정

--- routes/nodeDashboardEFSM.js
+++ routes/nodeDashboardEFSM.js
@@ -77,174 +77,19 @@ router.get('/restapi/getDashboardRawData', function(req, res, next) {
   }
 });
 
-router.get('/restapi/selectJiraAccId', function(req, res, next) {
-  console.log('sample/restapi/selectJiraAccReqDash');
-  var date = new Date().toString().split(' ');
-  var mon = {'Jan' : '01', 'Feb' : '02', 'Mar' : '03', 'Apr' : '04', 'May' : '05', 'Jun' : '06', 'Jul' : '07', 'Aug' : '08', 'Sep' : '09', 'Oct' : '10', 'Nov' : '11', 'Dec' : '12' };
-  var in_data = {
-    today: indexAcc+date[3]+"."+mon[date[1]]+"."+date[2]
-  };
-  queryProvider.selectSingleQueryByID2("dashboard","selectJiraAccId", in_data, function(err, out_data, params) {
-    // console.log(out_datsa);
-    var rtnCode = CONSTS.getErrData('0000');
-    if (out_data == null) {
-      rtnCode = CONSTS.getErrData('0001');
-     }
-     var node={}, nodes = [], line = {}, lines = [], req={},  last = {}, lineNode = {}, id={};
-     var colors=['#FF0000', '#FF5E00', '#FFBB00', '#FFE400', '#ABF200', '#1DDB16', '#00D8FF', '#0054FF', '#0100FF', '#5F00FF',
-                        '#FF00DD', '#FF007F', '#FFA7A7', '#FFE08C', '#CEF279', '#B2EBF4', '#B5B2FF', '#FFB2F5', '#CC723D', '#008299'];  
-     var reqCnt = 0, nodeCnt = 0, lineCnt = 0, lineNodeCnt = 0, idCnt = 0;        
-     var nodeNo = 0;
-     var nodeList = [];
-     out_data.forEach(function(d) {        
-        var a = d._source.request.split('?');                     
-        var b = a[0].split('/');
-
-        if(req[a[0]] == null) {          
-          req[a[0]] = { no : reqCnt++, cnt : 1};
-        } else {
-          req[a[0]].cnt++;        
-        }        
-        
-        if(id[b[b.length-1]] == null) {
-          id[b[b.length-1]] = colors[idCnt++%20];                   
-        }
-        
-        var nodeId = b[b.length-1]+'_'+req[a[0]].no;       
-        
-        if(node[nodeId] ==null){
-          nodeList[nodeNo] = nodeId;
-          node[nodeId] ={ name : a[0], id : nodeId, no : nodeNo++ };          
-        }
-        if(last[d._source.auth] != null){
-          var from = last[d._source.auth];
-          var to = nodeId;  
-          if(node[from].no > node[to].no){
-            from = nodeId;
-            to = last[d._source.auth];
-          }        
-          if(from != to){
-            if(line[node[to].no+'-'+node[from].no] == null){
-              if(lineNode[from] == null) {                
-                lineNode[from] = {};        
-                node[from].no = lineNodeCnt;        
-                nodes[lineNodeCnt++] = node[from];                        
-              }
-              if(lineNode[to] == null) {
-                lineNode[to] = {};    
-                node[to].no = lineNodeCnt;
-                nodes[lineNodeCnt++] = node[to];                                
-              }
-              var source = node[from].no;
-              var target = node[to].no;              
-              if(line[source+'-'+target] == null) {                
-                line[source+'-'+target] = { no : lineCnt };               
-                lines[lineCnt++] = {  source:  source , target: target, value : 0.0001, cnt :  1 };                
-              } else {                            
-                lines[line[source+'-'+target].no].value += 0.0001;
-                lines[line[source+'-'+target].no].cnt++;
-              }
-            } else {                   
-              lines[line[node[to].no+'-'+node[from].no].no].value += 0.0001;
-              lines[line[node[to].no+'-'+node[from].no].no].cnt++;
-            }
-          } else {            
-           if(lineNode[to] == null) {
-              lineNode[to] = {};                  
-              node[to].no = lineNodeCnt;              
-              nodes[lineNodeCnt++] = node[to];                                
-            }  
-          }
-        }        
-        last[d._source.auth] =  node[nodeId].id;       
-       });
-     var json = {"nodes" :nodes, "links" : lines };
-     var text = JSON.stringify(json);    
-    res.json({rtnCode: rtnCode, rtnData: json, id : id});
-  });
-});
-
-router.get('/restapi/selectJiraAccJson', function(req, res, next) {
-  console.log('dashboard/restapi/selectJiraAccJson');
-  var date = new Date().toString().split(' ');
-  var mon = {'Jan' : '01', 'Feb' : '02', 'Mar' : '03', 'Apr' : '04', 'May' : '05', 'Jun' : '06', 'Jul' : '07', 'Aug' : '08', 'Sep' : '09', 'Oct' : '10', 'Nov' : '11', 'Dec' : '12' };
-  var in_data = {
-    today: indexAcc+date[3]+"."+mon[date[1]]+"."+date[2]    
-  };  
+router.get('/restapi/selectJiraAccReq', function(req, res, next) {
+  console.log('dashboard/restapi/selectJiraAccReq');
+  var in_data = {    index : req.query.index, START : req.query.START, END : req.query.END  };  
   queryProvider.selectSingleQueryByID2("dashboard","selectJiraAccReq", in_data, function(err, out_data, params) {
     // console.log(out_datsa);
     var rtnCode = CONSTS.getErrData('0000');
     if (out_data == null) {
       rtnCode = CONSTS.getErrData('0001');
-     }
-     var node={}, nodes = [], line = {}, lines = [], req={},  last = {}, lineNode = {}, id= {};
-      var colors=['#61DBF0', '#f5662b', '#FAED7D', '#367d85', '#AB6CFF', '#97ba4c', '#3f3e47', '#9f9fa3', '#1F50B5', '#FFBB00'];
-     var reqCnt = 0, nodeCnt = 0, lineCnt = 0, lineNodeCnt = 0, idCnt = 0;        
-     var nodeNo = 0;
-     var nodeList = [];
-     out_data.forEach(function(d) {        
-        var a = d._source.request.split('?');                     
-        
-        if(req[a[0]] == null) {          
-          req[a[0]] = { no : reqCnt++, cnt : 1};
-        } else {
-          req[a[0]].cnt++;        
-        }
-
-        if(id[d._source.auth] == null) {
-          id[d._source.auth] = colors[idCnt++%10];                   
-        }
-        var nodeId = d._source.auth+'_'+req[a[0]].no;       
-        
-        if(node[nodeId] ==null){
-          nodeList[nodeNo] = nodeId;
-          node[nodeId] ={ name : a[0], id : nodeId, no : nodeNo++ };          
-        }
-        if(last[d._source.auth] != null){
-          var from = last[d._source.auth];
-          var to = nodeId;  
-          if(node[from].no > node[to].no){
-            from = nodeId;
-            to = last[d._source.auth];
-          }        
-          if(from != to){
-            if(line[node[to].no+'-'+node[from].no] == null){
-              if(lineNode[from] == null) {                
-                lineNode[from] = {};        
-                node[from].no = lineNodeCnt;        
-                nodes[lineNodeCnt++] = node[from];                        
-              }
-              if(lineNode[to] == null) {
-                lineNode[to] = {};    
-                node[to].no = lineNodeCnt;
-                nodes[lineNodeCnt++] = node[to];                                
-              }
-              var source = node[from].no;
-              var target = node[to].no;              
-              if(line[source+'-'+target] == null) {                
-                line[source+'-'+target] = { no : lineCnt };               
-                lines[lineCnt++] = {  source:  source , target: target, value : 0.0001 };                
-              } else {                            
-                lines[line[source+'-'+target].no].value += 0.0001;
-              }
-            } else {                   
-              lines[line[node[to].no+'-'+node[from].no].no].value += 0.0001;
-            }
-          } else {            
-           if(lineNode[to] == null) {
-              lineNode[to] = {};                  
-              node[to].no = lineNodeCnt;              
-              nodes[lineNodeCnt++] = node[to];                                
-            }  
-          }
-        }        
-        last[d._source.auth] =  node[nodeId].id;       
-       });
-     var json = {"nodes" :nodes, "links" : lines };
-     var text = JSON.stringify(json);    
-    res.json({rtnCode: rtnCode, rtnData: json, color : id});
+     }     
+    res.json({rtnCode: rtnCode, rtnData: out_data});
   });
 });
+
 
 router.get('/restapi/selectJiraAccScatter', function(req, res, next) {
   console.log('dashboard/restapi/selectJiraAccScatter');
--- routes/nodeSample.js
+++ routes/nodeSample.js
@@ -121,20 +121,6 @@ router.get('/restapi/selectJiraAccess', function(req, res, next) {
   });
 });
 
-// query Report
-router.get('/restapi/selectJiraAccReq', function(req, res, next) {
-  console.log('sample/restapi/selectJiraAccReq');
-  var in_data = {};
-  queryProvider.selectSingleQueryByID2("sample","selectJiraAccReq", in_data, function(err, out_data, params) {
-    // console.log(out_datsa);
-    var rtnCode = CONSTS.getErrData('0000');
-    if (out_data == null) {
-      rtnCode = CONSTS.getErrData('0001');
-    }
-    res.json({rtnCode: rtnCode, rtnData: out_data});
-  });
-});
-
 router.get('/restapi/selectJiraAccJson', function(req, res, next) {
   console.log('sample/restapi/selectJiraAccJson');
   var in_data = {   index : req.query.index };
@@ -149,9 +135,9 @@ router.get('/restapi/selectJiraAccJson', function(req, res, next) {
      var reqCnt = 0, nodeCnt = 0, lineCnt = 0, lineNodeCnt = 0, idCnt = 0;        
      var nodeNo = 0;
      var nodeList = [];
-     out_data.forEach(function(d) {        
+     out_data.forEach(function(d) {              
         var a = d._source.request.split('?');                     
-        
+        console.log(a)
         if(req[a[0]] == null) {          
           req[a[0]] = { no : reqCnt++, cnt : 1};
         } else {
@@ -214,7 +200,7 @@ router.get('/restapi/selectJiraAccJson', function(req, res, next) {
 
 });
 
-router.get('/restapi/selectJiraAccId', function(req, res, next) {
+router.get('/restapi/selectJiraAccReq', function(req, res, next) {
   console.log('sample/restapi/selectJiraAccId');
  var in_data = {   index : req.query.index };
   queryProvider.selectSingleQueryByID2("sample","selectJiraAccReq", in_data, function(err, out_data, params) {
@@ -222,78 +208,8 @@ router.get('/restapi/selectJiraAccId', function(req, res, next) {
     var rtnCode = CONSTS.getErrData('0000');
     if (out_data == null) {
       rtnCode = CONSTS.getErrData('0001');
-     }
-     var node={}, nodes = [], line = {}, lines = [], req={},  last = {}, lineNode = {}, id={};
-     var colors=['#FF0000', '#FF5E00', '#FFBB00', '#FFE400', '#ABF200', '#1DDB16', '#00D8FF', '#0054FF', '#0100FF', '#5F00FF',
-                        '#FF00DD', '#FF007F', '#FFA7A7', '#FFE08C', '#CEF279', '#B2EBF4', '#B5B2FF', '#FFB2F5', '#CC723D', '#008299'];  
-     var reqCnt = 0, nodeCnt = 0, lineCnt = 0, lineNodeCnt = 0, idCnt = 0;        
-     var nodeNo = 0;
-     var nodeList = [];
-     out_data.forEach(function(d) {        
-        var a = d._source.request.split('?');                     
-        var b = a[0].split('/');
-
-        if(req[a[0]] == null) {          
-          req[a[0]] = { no : reqCnt++, cnt : 1};
-        } else {
-          req[a[0]].cnt++;        
-        }        
-        
-        if(id[b[b.length-1]] == null) {
-          id[b[b.length-1]] = colors[idCnt++%20];                   
-        }
-        
-        var nodeId = b[b.length-1]+'_'+req[a[0]].no;       
-        
-        if(node[nodeId] ==null){
-          nodeList[nodeNo] = nodeId;
-          node[nodeId] ={ name : a[0], id : nodeId, no : nodeNo++ };          
-        }
-        if(last[d._source.auth] != null){
-          var from = last[d._source.auth];
-          var to = nodeId;  
-          if(node[from].no > node[to].no){
-            from = nodeId;
-            to = last[d._source.auth];
-          }        
-          if(from != to){
-            if(line[node[to].no+'-'+node[from].no] == null){
-              if(lineNode[from] == null) {                
-                lineNode[from] = {};        
-                node[from].no = lineNodeCnt;        
-                nodes[lineNodeCnt++] = node[from];                        
-              }
-              if(lineNode[to] == null) {
-                lineNode[to] = {};    
-                node[to].no = lineNodeCnt;
-                nodes[lineNodeCnt++] = node[to];                                
-              }
-              var source = node[from].no;
-              var target = node[to].no;              
-              if(line[source+'-'+target] == null) {                
-                line[source+'-'+target] = { no : lineCnt };               
-                lines[lineCnt++] = {  source:  source , target: target, value : 0.0001, cnt :  1 };                
-              } else {                            
-                lines[line[source+'-'+target].no].value += 0.0001;
-                lines[line[source+'-'+target].no].cnt++;
-              }
-            } else {                   
-              lines[line[node[to].no+'-'+node[from].no].no].value += 0.0001;
-              lines[line[node[to].no+'-'+node[from].no].no].cnt++;
-            }
-          } else {            
-           if(lineNode[to] == null) {
-              lineNode[to] = {};                  
-              node[to].no = lineNodeCnt;              
-              nodes[lineNodeCnt++] = node[to];                                
-            }  
-          }
-        }        
-        last[d._source.auth] =  node[nodeId].id;       
-       });
-     var json = {"nodes" :nodes, "links" : lines };
-     var text = JSON.stringify(json);    
-    res.json({rtnCode: rtnCode, rtnData: json, id : id});
+     }     
+    res.json({rtnCode: rtnCode, rtnData: out_data});
   });
 });
 
