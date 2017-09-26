c624305abae94c12bdc5a5bf3827acaab585ed11
Kihyun Hwang <kihyun.hwang@m2utech.com>
2017-09-26 18:13:06 +0900

    날짜 유틸 추가

---
+++ programk-kakao/src/main/java/m2u/eyelink/aibot/utils/DateUtils.java
@@ -0,0 +1,47 @@
+package m2u.eyelink.aibot.utils;
+
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.SimpleDateFormat;
+
+public class DateUtils {
+
+	public static final DateUtils instance =  new DateUtils();
+	private final DateTimeFormatter formatter_19 = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+	private final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
+	public String getCurrentDttm(String format) {
+		// any kind of format like ....yyyy-MM-dd HH:mm:ss 
+		SimpleDateFormat sdf = (format != null ? new SimpleDateFormat(format) : new SimpleDateFormat(DEFAULT_FORMAT));
+		return sdf.format(new Date());
+	}
+	
+	public int daysPassed(String dttm, int days) {
+		DateTime dt = DateTime.parse(dttm, formatter_19);
+		DateTime dtc = DateTime.now();
+		return dtc.compareTo(dt.plusDays(days));
+	}
+	
+	/**
+	 * 
+	 * @param dttm
+	 * @param minutes
+	 * @return 1 : dttm이 minutes(분)이상 경과했을 경우,
+	 * 		   0 : dttm이 정확히 minutes(분) 경과한 경우,
+	 * 		  -1 : dttm이 아직 minutes(분)을 경과하지 않은 경우
+	 */
+	public int minutuesPassed(String dttm, int minutes) {
+		DateTime dt = DateTime.parse(dttm, formatter_19);
+		DateTime dtc = DateTime.now();
+		return dtc.compareTo(dt.plusMinutes(minutes));
+	}
+	
+	public static void main(String[] args) {
+//		System.out.println( DateUtils.instance.getCurrentDttm("yyyy-MM-dd HH:mm:ss") );
+	}
+	
+}
