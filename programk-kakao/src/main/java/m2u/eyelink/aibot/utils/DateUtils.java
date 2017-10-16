59a19c0a5e03241e22588a119a94b5d398e81312
Kihyun Hwang <kihyun.hwang@m2utech.com>
2017-10-16 09:55:59 +0900

    신규 메서드 추가

--- programk-kakao/src/main/java/m2u/eyelink/aibot/utils/DateUtils.java
+++ programk-kakao/src/main/java/m2u/eyelink/aibot/utils/DateUtils.java
@@ -14,6 +14,10 @@ public class DateUtils {
 	private final DateTimeFormatter formatter_19 = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 	private final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
 	
+	public String getDefaultDttm(String format) {
+		SimpleDateFormat sdf = (format != null ? new SimpleDateFormat(format) : new SimpleDateFormat(DEFAULT_FORMAT));
+		return sdf.format(new Date(0));
+	}
 	public String getCurrentDttm(String format) {
 		// any kind of format like ....yyyy-MM-dd HH:mm:ss 
 		SimpleDateFormat sdf = (format != null ? new SimpleDateFormat(format) : new SimpleDateFormat(DEFAULT_FORMAT));
@@ -34,14 +38,14 @@ public class DateUtils {
 	 * 		   0 : dttm이 정확히 minutes(분) 경과한 경우,
 	 * 		  -1 : dttm이 아직 minutes(분)을 경과하지 않은 경우
 	 */
-	public int minutuesPassed(String dttm, int minutes) {
+	public int minutesPassed(String dttm, int minutes) {
 		DateTime dt = DateTime.parse(dttm, formatter_19);
 		DateTime dtc = DateTime.now();
 		return dtc.compareTo(dt.plusMinutes(minutes));
 	}
 	
 	public static void main(String[] args) {
-//		System.out.println( DateUtils.instance.getCurrentDttm("yyyy-MM-dd HH:mm:ss") );
+//		System.out.println( DateUtils.instance.getDefaultDttm("yyyy-MM-dd HH:mm:ss") );
 	}
 	
 }
