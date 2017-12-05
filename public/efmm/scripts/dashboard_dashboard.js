ae20687ffd9253c60b917ef07377232f1a32bcc1
ohliAnnie <ohliannie@naver.com>
2017-12-05 18:07:37 +0900

    efmm dashboard

--- public/efmm/scripts/dashboard_dashboard.js
+++ public/efmm/scripts/dashboard_dashboard.js
@@ -1,9 +1,8 @@
 $(document).ready(function(e) {        
-  getData();
-  drawGage(value);
+  drawGage();
+  getData();  
 });
 
-var value = { oee : 0, availability : 0, performance : 0, quality : 0 };
 var oee, availability, performance, quality;
 
 (function loop() {
@@ -13,45 +12,54 @@ var oee, availability, performance, quality;
 
 function getData(){
   var data = { now : new Date().getTime() };
-  var in_data = { url : "/dashboard/restapi/getDashboardGageData", type : "GET", data : data };
+  
+  var in_data = { url : "/dashboard/restapi/getDashboardWeekly", type : "GET", data : data };
   ajaxTypeData(in_data, function(result){  
-    if (result.rtnCode.code == "0000") {
+    if (result.rtnCode.code == "0000") {            
       var data = result.rtnData;
       console.log(data);
-      var notching = data.notching[data.notching.length-1];
-      var stacking = data.stacking[data.stacking.length-1];
-      drawLineChart(data.stacking.concat(data.notching));  
+      drawLineChart(data.total, data.week);
+      updateGage(data.total[data.total.length-1]);
+      var tdata = {};
+      tdata.date = in_data.data.now
+      tdata.kpis = { tot : 18, in_pro : 15, stop : 3, alarm : 3 };
+      tdata.tab_welding = { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3600, e_unit : 29500, p_unit : 28970, g_unit : 28889, n_unit : 81 };
+      tdata.packaging = { tot : 2, in_pro : 1, stop : 1, alarm : 1, a_time : 7200, r_time : 3540, e_unit : 29000, p_unit : 28700, g_unit : 28618, n_unit : 82 };
+      tdata.degassing = { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3590, e_unit : 28500, p_unit : 28470, g_unit : 28392, n_unit : 78 };
+      tdata.leak_inspection = { tot : 2, in_pro : 1, stop : 1, alarm : 1, a_time : 7200, r_time : 3570, e_unit : 28000, p_unit : 27590, g_unit : 27513, n_unit : 77 };
+      tdata.can_swaging = { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3600, e_unit : 27500, p_unit : 26990, g_unit : 26911, n_unit : 79 };
+      data.stacking.tot= 6;
+      data.stacking.in_pro = 5;
+      data.stacking.stop = 1;
+      data.stacking.alarm = 1;
+      data.notching.tot = 2;
+      data.notching.in_pro = 2;
+      data.notching.stop = '';
+      data.notching.alarm = '';
+      for(key in data){
+        tdata[key] = { tot : data[key].tot, in_pro : data[key].in_pro, stop : data[key].stop, alarm : data[key].alarm, a_time : Math.ceil(data[key].planned_production_time/60),
+          d_time : Math.ceil(data[key].total_down_time/60), r_time :Math.ceil(data[key].operating_time/60), e_unit : data[key].total_expected_unit,
+          p_unit : data[key].total_pieces, g_unit : data[key].total_accept_pieces, n_unit : data[key].total_reject_pieces, overall_oee : (data[key].overall_oee*100).toFixed(1),
+          availability : (data[key].availability*100).toFixed(1), performance : (data[key].performance*100).toFixed(1), quality : (data[key].quality*100).toFixed(1) };
+      }
+      drawTable(tdata);      
     } 
   }); 
-  var in_data = { url : "/dashboard/restapi/getDashboardAggsData", type : "GET", data : data };
-  ajaxTypeData(in_data, function(result){  
-    if (result.rtnCode.code == "0000") {
-      var data = result.rtnData;      
-      console.log(data);
-      var tData = {
-        date : data.notching.key_as_string,
-        kpis : { tot : 18, in_pro : 15, stop : 3, alarm : 3 },
-        notching : { tot : 6, in_pro : 5, stop : 1, alarm : 1, a_time : data.notching.availability_time.value, d_time : data.notching.down_time.value, r_time :data.notching.running_time.value, e_unit : data.notching.expected_unit.value, p_unit : data.notching.produce_unit.value, g_unit : data.notching.good_unit.value
-    , n_unit : data.notching.ng_unit.value },
-        stacking : { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : data.stacking.availability_time.value, d_time : data.stacking.down_time.value, r_time :data.stacking.running_time.value, e_unit : data.stacking.expected_unit.value, p_unit : data.stacking.produce_unit.value, g_unit : data.stacking.good_unit.value
-    , n_unit : data.stacking.ng_unit.value },
-        tab_welding : { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3600, e_unit : 29500, p_unit : 28970, g_unit : 28889, n_unit : 81 },
-        packaging : { tot : 2, in_pro : 1, stop : 1, alarm : 1, a_time : 7200, r_time : 3540, e_unit : 29000, p_unit : 28700, g_unit : 28618, n_unit : 82 },
-        degassing : { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3590, e_unit : 28500, p_unit : 28470, g_unit : 28392, n_unit : 78 },
-        leak_inspection : { tot : 2, in_pro : 1, stop : 1, alarm : 1, a_time : 7200, r_time : 3570, e_unit : 28000, p_unit : 27590, g_unit : 27513, n_unit : 77 },
-        can_swaging : { tot : 2, in_pro : 2, stop : '', alarm : '', a_time : 7200, r_time : 3600, e_unit : 27500, p_unit : 26990, g_unit : 26911, n_unit : 79 }    
-      };
-      drawTable(tData);      
-    } 
-  });   
 }
 
-function drawGage(value){  
+function drawGage(){  
   var max = 100; 
-  oee = getGaguChart("oee", max, '#F361DC', value["oee"], 0.15);
-  availability = getGaguChart("availability", max, 'blue', value["availability"], 0.1);
-  performance = getGaguChart("performance", max, '#FF7012', value["performance"], 0.1);
-  quality = getGaguChart("quality", max, 'green', value["quality"], 0.1);  
+  oee = getGaguChart("oee", max, '#F361DC', 0, 0.15);
+  availability = getGaguChart("availability", max, 'blue', 0, 0.1);
+  performance = getGaguChart("performance", max, '#FF7012', 0, 0.1);
+  quality = getGaguChart("quality", max, 'green', 0, 0.1);  
+}
+
+function updateGage(value){  
+  oee.update(value.overall_oee*100);
+  availability.update(value.availability*100);
+  performance.update(value.performance*100);
+  quality.update(value.quality*100);
 }
 
 function getGaguChart(id, max, color, value, size) {
@@ -75,159 +83,64 @@ function getGaguChart(id, max, color, value, size) {
   });
 }
 
-function drawLineChart(data) {
-  var minDate = new Date(data[0].dtSensed);
-  var maxDate = new Date(data[data.length-1].dtSensed);
-  /*var xData = [];
-  for(i=0; i<data.length; i++){
-    xData[i] = data[i].date;
-  }*/
+function drawLineChart(data, week) {
+
   var composite = dc.compositeChart("#composed");
   
   var ndx = crossfilter(data);
 
-  var dim  = ndx.dimension(function(d){    
-    //console.log(d.overall_oee, d.availability, d.performance, d.quality);
-    return new Date(d.dtSensed);
+  var dim  = ndx.dimension(function(d, v){
+    return v;
   });
 
-  var oGroup = dim.group().reduce(
-      function(p,v){                
-        p.count++;
-        p.sum += v.overall_oee;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-  //remove
-      function(p,v){
-        p.count--;    
-        p.sum -= v.overall_oee;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-      //init
-      function(p,v){
-         return {count:0, avg:0, sum:0};
-      }
-    );
-  var aGroup = dim.group().reduce(
-      function(p,v){        
-        p.count++;
-        p.sum += v.availability;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-  //remove
-      function(p,v){
-        p.count--;    
-        p.sum -= v.availability;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-      //init
-      function(p,v){
-         return {count:0, avg:0, sum:0};
-      }
-    );
-  var pGroup = dim.group().reduce(
-      function(p,v){        
-        p.count++;
-        p.sum += v.performance;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-  //remove
-      function(p,v){
-        p.count--;    
-        p.sum -= v.performance;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-      //init
-      function(p,v){
-         return {count:0, avg:0, sum:0};
-      }
-    );
-  var qGroup = dim.group().reduce(
-      function(p,v){        
-        p.count++;
-        p.sum += v.quality;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-  //remove
-      function(p,v){
-        p.count--;    
-        p.sum -= v.quality;
-        p.avg = p.sum/p.count;
-        return p;
-      },
-      //init
-      function(p,v){
-         return {count:0, avg:0, sum:0};
-      }
-    );
+  var oGroup = dim.group().reduceSum(function(d){ return d.overall_oee*100;});
+  var aGroup = dim.group().reduceSum(function(d){ return d.availability*100;});
+  var pGroup = dim.group().reduceSum(function(d){ return d.performance*100;});
+  var qGroup = dim.group().reduceSum(function(d){ return d.quality*100;});
   
+  var minDate = new Date(data[0].date);
+  var maxDate = new Date(data[data.length-1].date);
+
   composite.margins().bottom = 50;
   composite.margins().right = 65;
-  // composite.xAxis().tickFormat(function(v) {return xData[v];});
+  composite.xAxis().tickFormat(function(v) {return week[v];});
   composite.yAxis().ticks(7);
   composite
     .width(window.innerWidth*0.38)
     .height(290)
-    .x(d3.time.scale().domain([minDate,maxDate]))
-    //.x(d3.scale.linear().domain([0,6]))           
+    //.x(d3.time.scale().domain([minDate,maxDate]))
+    //.round(d3.time.day.round)
+    .x(d3.scale.linear().domain([0,6]))             
     .y(d3.scale.linear().domain([65, 102]))    
     .yAxisLabel("%")
     .legend(dc.legend().x(window.innerWidth*0.05).y(267).itemHeight(12).itemWidth(window.innerWidth*0.07).gap(4).horizontal(true))
     .renderHorizontalGridLines(true)
-    .title(function(d){            
-            return d.key.getFullYear()+'-'+(d.key.getMonth()+1)+'-'+d.key.getDate()+' : '+(d.value.avg*100).toFixed(1);
-            })
+    .title(function(d){ return this.layer+' : '+d.value; })
     .compose([
       dc.lineChart(composite)
           .dimension(dim)            
           .colors('#F361DC')
           .renderDataPoints(true)
-          .group(oGroup, "OEE")
-          .valueAccessor(function (p) {            
-            value.oee = p.value.avg*100;
-            return p.value.avg*100;
-          }),
+          .group(oGroup, "OEE"),
       dc.lineChart(composite)
           .dimension(dim)
           .colors('blue')
           .renderDataPoints(true)
-          .group(aGroup, "Availability")          
-          .valueAccessor(function (p) {            
-            value.availability = p.value.avg*100;
-            return p.value.avg*100;
-          }),
+          .group(aGroup, "Availability"),
       dc.lineChart(composite)
           .dimension(dim)
           .colors('#FF7012')
           .renderDataPoints(true)
-          .group(pGroup, "Performance")
-          .valueAccessor(function (p) {            
-            value.performance = p.value.avg*100;
-            return p.value.avg*100;
-          }),
+          .group(pGroup, "Performance"),
       dc.lineChart(composite)
           .dimension(dim)
           .colors('green')
           .renderDataPoints(true)
-          .group(qGroup, "Quality")            
-          .valueAccessor(function (p) {            
-            value.quality = p.value.avg*100;
-            return p.value.avg*100;
-          })
+          .group(qGroup, "Quality")                      
     ])
     .brushOn(false)
     .render();
-    oee.update(value.oee);
-    availability.update(value.availability);
-    performance.update(value.performance);
-    quality.update(value.quality);
+
 }
 
 var cnt = 0
@@ -264,6 +177,14 @@ function drawTable(data) {
   sb.append(drawTr(data, 'g_unit', 'black', 'right'));
   sb.append('<tr><th '+style+' colspan="2">NG Units</th>');
   sb.append(drawTr(data, 'n_unit', 'red', 'right'));
+  sb.append('<tr><th '+style+' colspan="2">Overall OEE</th>');
+  sb.append(drawTr(data, 'overall_oee', '#F361DC', 'right'));
+  sb.append('<tr><th '+style+' colspan="2">Avaiability</th>');
+  sb.append(drawTr(data, 'availability', 'blue', 'right'));
+  sb.append('<tr><th '+style+' colspan="2">Performance</th>');
+  sb.append(drawTr(data, 'performance', '#FF7012', 'right'));
+  sb.append('<tr><th '+style+' colspan="2">Quality</th>');
+  sb.append(drawTr(data, 'quality', 'green', 'right'));
   sb.append('</table>');  
 
   $('#tbody').append(sb.toString());    
