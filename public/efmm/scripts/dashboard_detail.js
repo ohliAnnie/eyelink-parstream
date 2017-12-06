9617c028d7f898433aab38558085655e39a150f9
ohliAnnie <ohliannie@naver.com>
2017-12-06 09:46:21 +0900

    click event 여부 추가

--- public/efmm/scripts/dashboard_detail.js
+++ public/efmm/scripts/dashboard_detail.js
@@ -34,28 +34,28 @@ function drawTable(data) {
   sb.append('<td '+style3+'>Tab Welding</td><td '+style3+'>Packaging</td><td '+style3+'>Degassing</td></tr><tr><td>');  
   var style = 'style="background-color:#9E9E9E;"';
   var style2 = 'text-align:center; background-color:#9E9E9E;';  
-  sb.append(innerTable(data.notching, 'notching'));
+  sb.append(innerTable(data.notching, 'notching', false));
   for(i=0; i<data.notch.length; i++) {
-    sb.append(innerTable(data.notch[i], 'notching'));
+    sb.append(innerTable(data.notch[i], 'notching', true));
   }
   sb.append('</td><td>');
-  sb.append(innerTable(data.stacking, 'stacking'));
+  sb.append(innerTable(data.stacking, 'stacking', false));
   for(i=0; i<data.stack.length; i++) {
-    sb.append(innerTable(data.stack[i], 'stacking'));
+    sb.append(innerTable(data.stack[i], 'stacking', true));
   }
   sb.append('</td><td>');
-  var testData = { availability : 91.3, performance : 82.1, quality : 99.7, overall_oee : 60 };
+  var testData = { availability : 0.913, performance : 0.821, quality : 0.997, overall_oee : 0.6 };
   testData.cid = 300;
   console.log(testData);
-  sb.append(innerTable(testData, 'welding'));
+  sb.append(innerTable(testData, 'welding', false));
   sb.append('</td><td>');
   testData.overall_oee = 30;
   testData.cid = 400;
-  sb.append(innerTable(testData, 'packaging'));
+  sb.append(innerTable(testData, 'packaging', false));
   sb.append('</td><td>');
   testData.overall_oee = 10;
   testData.cid = 500;
-  sb.append(innerTable(testData, 'degassing'));
+  sb.append(innerTable(testData, 'degassing', false));
   sb.append('</td></tr></table>');    
   $('#tbody').append(sb.toString());
   var gCnt = 0;
@@ -75,20 +75,22 @@ function drawTable(data) {
   console.log(gage);
 }
 
-function innerTable(data, type){
+function innerTable(data, type, event){
   //var style = 'style="color:'+color+'; text-align:'+align+'; font-weight:bold;"';
   var state = ['active', 'idle', 'alarm'];
   data.state = state[Math.floor(Math.random() * 10)%3]  
   var alarm = (data.state=='active')?'green':(data.state=='idle')?'blue':'red';
   var color = (data.overall_oee>=0.7)?'#6ABC64':(data.overall_oee>=0.5)?'#79ABFF':'#FF7E7E';  
-  var sb = '<table class="table table-striped table-bordered" onclick="location.href=';
-  sb += "'info?date="+getParameterByName('date')+'&type='+type+'&state='+data.state+'&cid='+data.cid+"'"+'">';
+  var sb = '<table class="table table-striped table-bordered"';
+  if(event){
+    sb += ' onclick="location.href='+"'info?date="+getParameterByName('date')+'&type='+type+'&state='+data.state+'&cid='+data.cid+"'"+'"';
+  }  
   console.log(sb);
-  sb += '<tr style="background-color:'+alarm+'"><td colspan="3" style="color:white; text-align:center;" >';
+  sb += '><tr style="background-color:'+alarm+'"><td colspan="3" style="color:white; text-align:center;" >';
   sb += '<strong style="font-size:18px;">'+data.cid+'</strong><br>'+data.state+'</td></tr>';    
   sb += '<tr><td colspan="3"><div class="gage'+cnt++ +'" style="text-align:center;"></div></td></tr>';
-  sb += '<tr style="background-color:'+color+'"><td>Ava<br>'+data.availability.toFixed(1)+'%</td><td>Perf<br>';
-  sb += data.performance.toFixed(1)+'%</td><td>Qual<br>'+data.quality.toFixed(1)+'%</td></tr>';
+  sb += '<tr style="background-color:'+color+'"><td>Ava<br>'+(data.availability*100).toFixed(1)+'%</td><td>Perf<br>';
+  sb += (data.performance*100).toFixed(1)+'%</td><td>Qual<br>'+(data.quality*100).toFixed(1)+'%</td></tr>';
   sb += '</table>'  
   return sb;
 }
