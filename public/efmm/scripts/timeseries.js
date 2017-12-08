3dac22ee89c948b7b7bd52b0a52fcdeb3bb36115
ohliAnnie <ohliannie@naver.com>
2017-12-08 11:18:37 +0900

    efmm timeseries ing

--- public/efmm/scripts/timeseries.js
+++ public/efmm/scripts/timeseries.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-  var dateFormat = 'YYYY-MM-DD';
+  var dateFormat = 'YYYY-MM-DD HH:mm:ss';  
+  $('#baseTime').val(moment().subtract(parseInt($('#gap').val()),'minutes').format(dateFormat));
   //$('#sdate').val(moment().format(dateFormat));   
   var start = new Date().getTime()-60*1000;  
-  $('#datetimepicker').datetimepicker();
+  //$('#datetimepicker').datetimepicker();
   // time series char를 그린다.
+  
   getData();
-
   $('#btn_search').click(function() {
     getData();
   });
 });
 
-function getData() {
+var chartData = {};
+function getData() {  
+  var data = { start : $('#baseTime').val(), gap : $('#gap').val()};  
+  var in_data = { url : "/timeseries/restapi/getTimeseries", type : "GET", data : data };
+  ajaxTypeData(in_data, function(result){  
+    if (result.rtnCode.code == "0000") {            
+    } 
+  });
   var dateFormat = 'YYYY-MM-DD';
   var rawData = [  
   { index : 0, date : moment().subtract(6, 'days').format(dateFormat), oee : 88, availability : 93, performance : 97, quality : 99},
@@ -21,7 +29,7 @@ function getData() {
   { index : 4, date : moment().subtract(2, 'days').format(dateFormat), oee : 77, availability : 88, performance : 87, quality : 99},
   { index : 5, date : moment().subtract(1, 'days').format(dateFormat), oee : 81, availability : 91, performance : 89, quality : 99},
   { index : 6, date : moment().format(dateFormat), oee : 87, availability : 91, performance : 90, quality : 99}];
-  drawTimeseries(rawData);
+  //drawTimeseries(rawData);
 }
 
 function drawTimeseries(data) {
@@ -50,6 +58,9 @@ function drawTimeseries(data) {
 
 }
 
+function chart01(){
+  console.log($('#cid1').val());
+}
 
 function drawChart01(data, chartName, xValue, vList){
   console.log(vList)
