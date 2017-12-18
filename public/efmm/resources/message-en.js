29c919287a0a3bbe37c9fdb0c110eabeb935d57c
TovenBae <furtur3@gmail.com>
2017-12-18 17:52:55 +0900

    developing, client UI message -> 공통화 처리

---
+++ public/efmm/resources/message-en.js
@@ -0,0 +1,21 @@
+var m;
+
+m = {
+  common : {
+    confirm: {
+      save : 'Do you want to save?',
+      delete : 'Do you want to delete?'
+    },
+    check: {
+      blank : '',
+      id : 'Please, input Id',
+      name : 'Please, input Name'
+    }
+  },
+  dashboard : {
+
+  },
+  recipe : {
+    check_id : 'Check ID를 수행해 주세요.'
+  }
+}
