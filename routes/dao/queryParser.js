25f93d4b6b9443f09babcbc5be62626df444275f
TovenBae <furtur3@gmail.com>
2018-01-11 18:35:34 +0900

    fixed, logging, efsm scheduler

--- routes/dao/queryParser.js
+++ routes/dao/queryParser.js
@@ -1,28 +1,29 @@
+var logger = global.log4js.getLogger('queryParser');
 module.exports.getQuery = function(type, queryId) {
-  // console.log('queryParser/getQuery -> global.queryList : %j', global.query.queryList);
-  // console.log('queryParser/getQuery -> global.query.%s query length : %d', type, global.query.queryList[''+ type +''][0].query.length);
+  // logger.debug('getQuery -> global.queryList : %j', global.query.queryList);
+  // logger.debug('getQuery -> global.query.%s query length : %d', type, global.query.queryList[''+ type +''][0].query.length);
   var query = '';
   if (global.query.queryList[''+type+''] == undefined) {
-    console.error('ERROR] queryParser : type [%s] is not found!!', type);
+    logger.error('type [%s] is not found!!', type);
 
   } else {
     for(var i = 0; i < global.query.queryList[''+type+''][0].query.length; i++) {
-      // console.log('queryParser/getQuery -> %d, %s', i, global.query.queryList[''+type+''][0].query[i].$.id);
+      // logger.debug('getQuery -> %d, %s', i, global.query.queryList[''+type+''][0].query[i].$.id);
       if (global.query.queryList[''+type+''][0].query[i].$.id == queryId) {
         query = global.query.queryList[''+type+''][0].query[i]._;
-        // console.log('queryParser/getQuery -> %s', query);
+        // logger.debug('getQuery -> %s', query);
         break;
       }
     }
     if (query == '') {
-      console.error('ERROR] queryParser : type [%s], queryId [%s] is not found!!', type, queryId);
+      logger.error('type [%s], queryId [%s] is not found!!', type, queryId);
     }
-     // console.log('queryParser/getQuery -> [%s]', query);
+     // logger.debug('getQuery -> [%s]', query);
     query = query.replace(/\r\n?/g, '').trim();
     query = query.replace(/\n?/g, '').trim();
 
   }
-  // console.log('queryParser/getQuery -> [%s]', query);
+  // logger.debug('getQuery -> [%s]', query);
   return query;
 }
 
